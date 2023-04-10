57bf1b9f2a2df7306daa23d2ec7cf6a084a32991
Valentin Markov <101991279+valyo132@users.noreply.github.com>
2023-04-10 17:24:26 +0300

    E-mail sender completed
    When you make a new registration the app sends you an e-mail.

--- NoteTaking.Web.Common/EmailSender.cs
+++ NoteTaking.Web.Common/EmailSender.cs
@@ -1,15 +1,32 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
-
+using System.Net;
+using System.Net.Mail;
 namespace NoteTaking.Web.Common
 {
     /// <summary>
-    /// Email sender.
+    /// E-mail sender.
     /// </summary>
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        /// <summary>
+        /// Sends the e-mail
+        /// </summary>
+        /// <param name="email">Receiver</param>
+        /// <param name="subject">Main subject of the e-mail</param>
+        /// <param name="htmlMessage">E-mail message</param>
+        /// <returns></returns>
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            return Task.CompletedTask;
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("valentinmarkov132@gmail.com", "xyswbhjqlhukesbg"),
+                EnableSsl = true,
+            };
+
+            MailMessage mailMessage = new MailMessage("valentinmarkov132@gmail.com", email, subject, htmlMessage);
+            mailMessage.IsBodyHtml = true;
+            smtpClient.Send(mailMessage);
         }
     }
 }
